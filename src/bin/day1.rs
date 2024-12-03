33766b1b1c4d6bfe4680820cb51555ede43af6d4
Jalen <jalenh405@gmail.com>
2024-12-02 20:38:44 -0500

    day 2 part 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,8 +1,19 @@
 use std::collections::HashMap;
 use std::fs::read_to_string;
 
+fn main() {
+    let file_path = "/Users/jalen/projects/aoc/data/day1.txt";
+    let data = read_to_string(file_path).expect("failed to read input");
+    let (list_1, list_2) = process_input(data);
+
+    let p1_solution = part_1(list_1.clone(), list_2.clone());
+    println!("Part 1 solution: {}", p1_solution);
+
+    let p2_solution = part_2(list_1, list_2);
+    println!("Part 2 solution: {}", p2_solution)
+}
+
 /// takes in the raw input and returns two sorted vectors.
-/// preserves the order of the input lists
 fn process_input(input: String) -> (Vec<i64>, Vec<i64>) {
     let (mut list_1, mut list_2): (Vec<i64>, Vec<i64>) = input
         .lines()
@@ -42,16 +53,4 @@ fn part_2(list_1: Vec<i64>, list_2: Vec<i64>) -> i64 {
         .sum();
 
     result
-}
-
-fn main() {
-    let file_path = "/Users/jalen/projects/aoc/data/day1.txt";
-    let data = read_to_string(file_path).expect("failed to read input");
-    let (list_1, list_2) = process_input(data);
-
-    let p1_solution = part_1(list_1.clone(), list_2.clone());
-    println!("Part 1 solution: {}", p1_solution);
-
-    let p2_solution = part_2(list_1, list_2);
-    println!("Part 2 solution: {}", p2_solution)
 }
---
+++ src/bin/day2.rs
@@ -0,0 +1,62 @@
+use std::fs::read_to_string;
+
+fn main() {
+    let file_path = "/Users/jalen/projects/aoc/data/day2.txt";
+    let data = read_to_string(file_path).expect("failed to read input");
+
+    let input = process_input(data);
+
+    let p1_solution = part_1(input);
+    println!("Part 1 solution: {}", p1_solution);
+
+    // let p2_solution = part_2(list_1, list_2);
+    // println!("Part 2 solution: {}", p2_solution)
+}
+
+fn process_input(input: String) -> Vec<Vec<i64>> {
+    input
+        .lines()
+        .filter_map(|line| {
+            if line.is_empty() {
+                None
+            } else {
+                let values: Vec<i64> = line
+                    .split_whitespace()
+                    .map(|s| s.parse::<i64>().expect("expected integer value"))
+                    .collect();
+
+                Some(values)
+            }
+        })
+        .collect()
+}
+
+fn is_safe_report(report: &[i64]) -> bool {
+    if report.len() < 2 {
+        return true;
+    }
+
+    let mut pairs = report.windows(2);
+
+    if let Some(first_pair) = pairs.next() {
+        let first_diff = first_pair[1] - first_pair[0];
+
+        if first_diff.abs() < 1 || first_diff.abs() > 3 {
+            return false;
+        }
+
+        let sign = first_diff.signum();
+
+        for pair in pairs {
+            let diff = pair[1] - pair[0];
+            if diff.abs() < 1 || diff.abs() > 3 || diff.signum() != sign {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+fn part_1(input: Vec<Vec<i64>>) -> i64 {
+    input.iter().filter(|report| is_safe_report(report)).count() as i64
+}
